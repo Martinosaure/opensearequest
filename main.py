e130654c2ae5d41ee4a6112c72c4326598e9c5ad
Martinosaure <64969683+Martinosaure@users.noreply.github.com>
2022-09-09 10:07:55 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+import requests
+from requests import Session
+import json
+
+
+
+url = 'https://api.opensea.io/api/v1/asset/0x8a90cab2b38dba80c64b7734e58ee1db38b8992e/1094/'
+headers = {
+    "Accept": "application/json",
+    "X-API-KEY": ""
+}
+r = requests.get(url, headers=headers,)
+data = r.json()['collection']
+
+# data = r.json()['average_price'] -> Returns  data = r.json()['average_price'] KeyError: 'average_price'
+
+print(data)
